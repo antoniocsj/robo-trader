9b6a06cc36496639860744b4038199a012c4b966
Antonio CSJ <acsj369@gmail.com>
2023-06-14 21:53:01 -0300

    ---

--- src/testes.py
+++ src/testes.py
@@ -34,22 +34,25 @@ def test_03():
     import pickle
     import json
     import numpy as np
+    from sklearn.preprocessing import MinMaxScaler
 
-    # class MinMaxScalerEncoder(json.JSONEncoder):
-    #     def default(self, obj):
-    #         if isinstance(obj, np.integer):
-    #             return int(obj)
-    #         if isinstance(obj, np.floating):
-    #             return float(obj)
-    #         if isinstance(obj, np.ndarray):
-    #             return obj.tolist()
-    #         return super(MinMaxScalerEncoder, self).default(obj)
+    class MinMaxScalerEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, MinMaxScaler):
+                return obj.__dict__
+            if isinstance(obj, np.integer):
+                return int(obj)
+            if isinstance(obj, np.floating):
+                return float(obj)
+            if isinstance(obj, np.ndarray):
+                return obj.tolist()
+            return super(MinMaxScalerEncoder, self).default(obj)
 
     with open('scalers.pkl', 'rb') as file:
         scalers = pickle.load(file)
 
-    # with open('scalers.json', 'w') as file:
-    #     json.dump(scalers, file, indent=4, sort_keys=False, cls=MinMaxScalerEncoder)
+    with open('scalers.json', 'w') as file:
+        json.dump(scalers, file, indent=4, sort_keys=False, cls=MinMaxScalerEncoder)
 
 
 if __name__ == '__main__':
