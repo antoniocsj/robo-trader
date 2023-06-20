db3b2446a406bf52d985486a664182ddcba0b9cc
Antonio CSJ <acsj369@gmail.com>
2023-06-20 13:45:20 -0300

    ---

--- src/DirectoryCorrection3.py
+++ src/DirectoryCorrection3.py
@@ -362,7 +362,7 @@ class DirectoryCorrection:
                     self.sheets_exclude_last_rows(s.current_row)
                     break
 
-            if _current_row % 1000 == 0 and _current_row > 0:
+            if _current_row % 5000 == 0 and _current_row > 0:
                 self.save_sheets(print_row='current')
                 self.write_checkpoint(index_proc)
                 print(f'{100 * _current_row / _max_len: .2f} %\n')
