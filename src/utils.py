aa19a8cd64ba3b0e0279b2ad3518402d282ab9d3
Antonio CSJ <acsj369@gmail.com>
2023-06-26 18:49:42 -0300

    ---

--- src/utils.py
+++ src/utils.py
@@ -408,7 +408,7 @@ def save_train_configs(_train_configs: dict):
 
 
 # usada nas criação de amostra de treinamento das redes neurais
-def prepare_train_data_multi(_hist: HistMulti, _symbol_out: str, _start_index: int,
+def prepare_train_data_multi_0(_hist: HistMulti, _symbol_out: str, _start_index: int,
                              _num_velas: int, _tipo_vela: str) -> ndarray:
     _data = []
     _timeframe = _hist.timeframe
@@ -459,6 +459,84 @@ def prepare_train_data_multi(_hist: HistMulti, _symbol_out: str, _start_index: i
                 _data = _ohlcv_in
             else:
                 _data = np.hstack((_data, _ohlcv_in))
+
+        _c_out = _hist.arr[_symbol_tf_out][_start_index + 1:_start_index + _num_velas + 1][:, 5]
+        _c_out = _c_out.reshape(len(_c_out), 1)
+        _data = np.hstack((_data, _c_out))
+    else:
+        print(f'tipo de vela não suportado: {_tipo_vela}')
+        exit(-1)
+
+    return _data
+
+
+# usada nas criação de amostra de treinamento das redes neurais
+def prepare_train_data_multi(_hist: HistMulti, _symbol_out: str, _start_index: int,
+                             _num_velas: int, _tipo_vela: str) -> ndarray:
+    _data = []
+    _timeframe = _hist.timeframe
+    _symbol_tf_out = f'{_symbol_out}_{_timeframe}'
+
+    if _tipo_vela == 'C':
+        for _symbol in _hist.symbols:
+            _symbol_timeframe = f'{_symbol}_{_timeframe}'
+            if _hist.arr[_symbol_timeframe].shape[1] == 3:
+                _data_in = _hist.arr[_symbol_timeframe][_start_index:_start_index + _num_velas][:, 2]
+            else:
+                _data_in = _hist.arr[_symbol_timeframe][_start_index:_start_index + _num_velas][:, 5]
+            _data_in = _data_in.reshape(len(_data_in), 1)
+            if len(_data) == 0:
+                _data = _data_in
+            else:
+                _data = np.hstack((_data, _data_in))
+
+        _c_out = _hist.arr[_symbol_tf_out][_start_index + 1:_start_index + _num_velas + 1][:, 5]
+        _c_out = _c_out.reshape(len(_c_out), 1)
+        _data = np.hstack((_data, _c_out))
+    elif _tipo_vela == 'CV':
+        for _symbol in _hist.symbols:
+            _symbol_timeframe = f'{_symbol}_{_timeframe}'
+            if _hist.arr[_symbol_timeframe].shape[1] == 3:
+                _data_in = _hist.arr[_symbol_timeframe][_start_index:_start_index + _num_velas][:, 2]
+                _data_in = _data_in.reshape(len(_data_in), 1)
+            else:
+                _data_in = _hist.arr[_symbol_timeframe][_start_index:_start_index + _num_velas][:, 5:7]
+            if len(_data) == 0:
+                _data = _data_in
+            else:
+                _data = np.hstack((_data, _data_in))
+
+        _c_out = _hist.arr[_symbol_tf_out][_start_index + 1:_start_index + _num_velas + 1][:, 5]
+        _c_out = _c_out.reshape(len(_c_out), 1)
+        _data = np.hstack((_data, _c_out))
+    elif _tipo_vela == 'OHLC':
+        for _symbol in _hist.symbols:
+            _symbol_timeframe = f'{_symbol}_{_timeframe}'
+            if _hist.arr[_symbol_timeframe].shape[1] == 3:
+                _data_in = _hist.arr[_symbol_timeframe][_start_index:_start_index + _num_velas][:, 2]
+                _data_in = _data_in.reshape(len(_data_in), 1)
+            else:
+                _data_in = _hist.arr[_symbol_timeframe][_start_index:_start_index + _num_velas][:, 2:6]
+            if len(_data) == 0:
+                _data = _data_in
+            else:
+                _data = np.hstack((_data, _data_in))
+
+        _c_out = _hist.arr[_symbol_tf_out][_start_index + 1:_start_index + _num_velas + 1][:, 5]
+        _c_out = _c_out.reshape(len(_c_out), 1)
+        _data = np.hstack((_data, _c_out))
+    elif _tipo_vela == 'OHLCV':
+        for _symbol in _hist.symbols:
+            _symbol_timeframe = f'{_symbol}_{_timeframe}'
+            if _hist.arr[_symbol_timeframe].shape[1] == 3:
+                _data_in = _hist.arr[_symbol_timeframe][_start_index:_start_index + _num_velas][:, 2]
+                _data_in = _data_in.reshape(len(_data_in), 1)
+            else:
+                _data_in = _hist.arr[_symbol_timeframe][_start_index:_start_index + _num_velas][:, 2:7]
+            if len(_data) == 0:
+                _data = _data_in
+            else:
+                _data = np.hstack((_data, _data_in))
 
         _c_out = _hist.arr[_symbol_tf_out][_start_index + 1:_start_index + _num_velas + 1][:, 5]
         _c_out = _c_out.reshape(len(_c_out), 1)
