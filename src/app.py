9d304189da7d1ac64a11bdc46c6deb1de75dae45
Antonio CSJ <acsj369@gmail.com>
2023-07-01 13:33:05 -0300

    ---

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -14,6 +15,15 @@ def make_prediction():
     print('make_prediction()')
     data = request.json
     print(data)
+
+    print(data['last_datetime'])
+    _last_datetime = datetime.fromisoformat(data['last_datetime'])
+    print(f'last_datetime = {_last_datetime}')
+
+    print(data['trade_server_datetime'])
+    _trade_server_datetime = datetime.fromisoformat(data['trade_server_datetime'])
+    print(f'trade_server_datetime = {_trade_server_datetime}')
+
     write_json('request.json', data)
     return "OK"
 
