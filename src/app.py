264aa736c8e6ca8382bdfdfa6a47293200de4c73
Antonio CSJ <acsj369@gmail.com>
2023-07-01 12:29:50 -0300

    ---

--- src/app.py
+++ src/app.py
@@ -1,13 +1,20 @@
 from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
 
+def write_json(_filename: str, _dict: dict):
+    with open(_filename, 'w') as file:
+        json.dump(_dict, file, indent=4)
+
+
 @app.route('/', methods=['POST'])
-def hello_world():
-    print('hello_world()')
+def make_prediction():
+    print('make_prediction()')
     data = request.json
     print(data)
+    write_json('request.json', data)
     return "OK"
 
 
