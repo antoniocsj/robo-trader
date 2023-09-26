aa56c6ae0d454d090effdbab50ce954fb88fdfdb
Antonio CSJ <acsj369@gmail.com>
2023-09-26 13:01:11 -0300

    ---

--- src/neural_robot_01.py
+++ src/neural_robot_01.py
@@ -85,7 +85,7 @@ def train_model():
         exit(-1)
 
     n_steps = 8
-    n_hidden_layers = 2
+    n_hidden_layers = 3
     n_samples_train = 199000  # Número de amostras usadas na fase de treinamento e validação
     validation_split = 0.2
     n_samples_test = 3000  # Número de amostras usadas na fase de avaliação. São amostras inéditas.
