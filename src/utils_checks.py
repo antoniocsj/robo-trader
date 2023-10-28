02c0e2fb5849a20d0b0b9f3bf88559d424c431f4
Antonio CSJ <acsj369@gmail.com>
2023-10-28 17:34:29 -0300

    ---

--- src/utils_checks.py
+++ src/utils_checks.py
@@ -17,31 +17,29 @@ def initial_compliance_checks():
     temp_dir = settings['temp_dir']
     symbol_out = settings['symbol_out']
 
-    params_nn_timeframe = params_nn['timeframe']
-    params_nn_candle_input_type = params_nn['candle_input_type']
+    params_tf = params_nn['timeframe']
+    params_cit = params_nn['candle_input_type']
 
-    settings_timeframe = settings['timeframe']
-    settings_candle_input_type = settings['candle_input_type']
+    settings_tf = settings['timeframe']
+    settings_cit = settings['candle_input_type']
 
-    if params_nn_timeframe != settings_candle_input_type:
-        print(f'ERRO. params_nn_timeframe ({params_nn_timeframe}) != '
-              f'settings_candle_input_type ({settings_candle_input_type})')
+    if params_tf != settings_tf:
+        print(f'ERRO. params_nn_timeframe ({params_tf}) != settings_timeframe ({settings_tf})')
         exit(-1)
 
-    if params_nn_candle_input_type != settings_timeframe:
-        print(f'ERRO. params_nn_candle_input_type ({params_nn_candle_input_type}) != '
-              f'settings_timeframe ({settings_timeframe})')
+    if params_cit != settings_cit:
+        print(f'ERRO. params_nn_candle_input_type ({params_cit}) != settings_candle_input_type ({settings_cit})')
         exit(-1)
 
-    hist = HistMulti(temp_dir, settings_timeframe, symbols_allowed=[symbol_out])
+    hist = HistMulti(temp_dir, settings_tf, symbols_allowed=[symbol_out])
 
     if symbol_out not in hist.symbols:
         print(f'ERRO. symbol_out ({symbol_out}) not in hist.symbols ({hist.symbols})')
         exit(-1)
 
-    if hist.timeframe != settings_timeframe:
+    if hist.timeframe != settings_tf:
         print(f'o timeframe do diretório {temp_dir} ({hist.timeframe}) é diferente do timeframe especificado '
-              f'em settings.json ({settings_timeframe})')
+              f'em settings.json ({settings_tf})')
         exit(-1)
 
     del hist
@@ -50,7 +48,7 @@ def initial_compliance_checks():
     with open('scalers.pkl', 'rb') as file:
         scalers: dict = pickle.load(file)
 
-    symbol_timeframe = f'{symbol_out}_{settings_timeframe}'
+    symbol_timeframe = f'{symbol_out}_{settings_tf}'
     if symbol_timeframe not in scalers:
         print(f'ERRO. symbol_timeframe ({symbol_timeframe}) not in scalers.pkl ({scalers.keys()}).')
         exit(-1)
