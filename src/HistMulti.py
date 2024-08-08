ca06f2ab425661c444103c351cb354cc99110f89
Antonio CSJ <acsj369@gmail.com>
2024-08-08 14:57:25 -0300

    feat: Add flexibility to HistMulti class
    The `HistMulti` class has been updated to add flexibility in loading historical data. The `source` parameter now accepts either a directory path or a dictionary of sheets. Additionally, the `timeframe`, `symbols_allowed`, and `symbolout` parameters have been added to customize the loading process. This change improves the versatility of the class and enhances its functionality.

--- src/HistMulti.py
+++ src/HistMulti.py
@@ -14,10 +14,11 @@ from src.utils.utils_symbols import search_symbols_in_dict
 class HistMulti:
     def __init__(self, source: Any, timeframe: str, symbols_allowed=None, symbolout=None):
         """
-        Cria um objeto que armazena os dados históricos obtidos por:
-         1) a partir de um diretório contendo arquivos CSVs ou ;
-         2) a partir de uma lista de objetos Sheets (Planilhas);
-        :param source: filepath (str) ou dict[Sheet]
+        Carrega os dados históricos de um diretório ou de um dicionário de planilhas.
+        :param source: diretório onde se encontra os arquivos csv ou um dicionário de planilhas.
+        :param timeframe: timeframe dos arquivos csv.
+        :param symbols_allowed: lista dos símbolos que devem ser carregados. Se None, carrega todos.
+        :param symbolout: se for especificado, este símbolo ficará em primeiro lugar na lista dos símbolos.
         """
         self.symbols = []
         self.timeframe = timeframe
@@ -254,80 +255,6 @@ class HistMulti:
             else:
                 count += len(candle_input_type)
         return count
-
-
-class HistMultiOriginal:
-    def __init__(self, directory: str):
-        self.directory = directory  # diretório onde se encontra os arquivos csv
-        self.all_files = []
-        self.csv_files = {}  # guarda os nomes dos arquivos csv conforme seu 'simbolo' e 'timeframe'
-        self.symbols = []
-        self.timeframe = ''
-        self.hist_csv = {}  # guarda os nomes dos arquivos csv conforme seu 'simbolo' e 'timeframe'
-        self.arr = {}  # guarda os arrays dos dados históricos conforme seu 'simbolo' e 'timeframe'
-
-        self.search_symbols()
-        self.load_symbols()
-
-    def search_symbols(self):
-        """
-        Procurando pelos arquivos csv correspondentes ao 'simbolo' e ao 'timeframe'
-        :return:
-        """
-        # passe por todos os arquivos csv e descubra o symbol e timeframe
-        self.all_files = os.listdir(self.directory)
-        for filename in self.all_files:
-            if filename.endswith('.csv'):
-                _symbol = filename.split('_')[0]
-                _timeframe = filename.split('_')[1]
-                if _timeframe.endswith('.csv'):
-                    _timeframe = _timeframe.replace('.csv', '')
-
-                if _symbol not in self.symbols:
-                    self.symbols.append(_symbol)
-                else:
-                    print(f'erro. o símbolo {_symbol} aparece repetido no mesmo diretório')
-                    exit(-1)
-
-                if self.timeframe == '':
-                    self.timeframe = _timeframe
-                elif _timeframe != self.timeframe:
-                    print(f'erro. há mais de um timeframe no mesmo diretório')
-                    exit(-1)
-
-                self.csv_files[f'{_symbol}_{_timeframe}'] = filename
-
-        self.symbols = sorted(self.symbols)
-
-    def get_csv_filepath(self, _symbol_timeframe: str) -> str:
-        _filepath = self.directory + '/' + self.csv_files[_symbol_timeframe]
-        return _filepath
-
-    def add_hist_data(self, _symbol: str, _timeframe: str):
-        _filepath = self.get_csv_filepath(f'{_symbol}_{_timeframe}')
-        key = f'{_symbol}_{_timeframe}'
-
-        df: DataFrame = pd.read_csv(_filepath, delimiter='\t')
-        # df.drop(columns=['<VOL>', '<SPREAD>'], inplace=True)
-        if df.isnull().sum().values.sum() != 0:
-            print(f'Há dados faltando no arquivo {_filepath}')
-            exit(-1)
-
-        self.arr[key] = df.to_numpy(copy=True)
-        # print(f'{key} carregando dados a partir de {_filepath}. {len(self.arr[key])} linhas')
-        del df
-
-    def load_symbols(self):
-        for s in self.symbols:
-            self.add_hist_data(s, self.timeframe)
-
-    def print_hist(self):
-        print('primeiras linhas dos históricos:')
-        if len(self.arr) > 0:
-            for k, v in enumerate(self.arr):
-                print(k, v, self.arr[v][0])
-        else:
-            print('os dados históricos não foram carregados ainda.')
 
 
 if __name__ == '__main__':
